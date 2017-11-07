825dc5f7b6a0053b499cce386ca062d5dbda931d
Derek Arnold <d.arnold7@gmail.com>
2017-11-07 12:16:15 -0500

    Updated to use Django templating

--- demo/settings.py
+++ demo/settings.py
@@ -119,8 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'app/static'),
+    os.path.join(
+        os.path.dirname(__file__),
+        'static',
+    ),
 )
 
