c477d0f635394ac87ed37f0611c955575673ead4
Michael Jenkins <michael.jenkins@disney.com>
2017-11-08 22:22:27 -0800

    Add registration

--- demo/urls.py
+++ demo/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^account/', include('registration.backends.simple.urls')),
     url(r'', include('app.urls')),
 ]
