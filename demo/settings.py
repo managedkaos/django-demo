a371a030827db189cab228c7375ea17d162d1d67
Michael Jenkins <michael.jenkins@disney.com>
2017-11-09 19:35:27 -0800

    add bare files

--- demo/settings.py
+++ demo/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['127.0.0.1', 'managedkaos.pythonanywhere.com']
 
 INSTALLED_APPS = [
     'app',
+    'bare',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -119,7 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
     os.path.join(
         os.path.dirname(__file__),
--- demo/urls.py
+++ demo/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^account/', include('registration.backends.simple.urls')),
     url(r'', include('app.urls')),
+    url(r'^bare/', include('bare.urls')),
 ]
